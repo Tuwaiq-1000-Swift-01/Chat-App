f80637b2586ce963ba8a68a001971864981aa812
amal <amal.141726@gmail.com>
2022-01-14 22:24:21 +0300

    Chat-App

---
+++ mssnger/mssnger/data.swift
@@ -0,0 +1,35 @@
+//
+//  data.swift
+//  mssnger
+//
+//  Created by Amal on 10/06/1443 AH.
+//
+
+import Foundation
+import Firebase
+struct Message {
+    let content : String?
+    let sender : String?
+    let reciever : String?
+    let id : String?
+    let timestamp : Timestamp
+    
+    func getNiceDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        return dateFormatter.string(from: timestamp.dateValue())
+    }
+}
+
+struct User {
+    let name : String?
+    let status : String?
+    let email : String?
+    var id : String?
+    let img : String?
+}
+
+
+
