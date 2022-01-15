05be4938469babeabdea6c76902f41e0fb8f86c4
Atheer-95 <athos@outlook.sa>
2022-01-15 18:01:27 +0300

    Chat App Completed

---
+++ AtheerChat/AtheerChat/controllers/TabViewController.swift
@@ -0,0 +1,42 @@
+//
+//  tabVCViewController.swift
+//  AtheerChat
+//
+//  Created by Eth Os on 06/04/1443 AH.
+//
+
+import UIKit
+import FirebaseAuth
+
+class TabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+           UITabBar.appearance().barTintColor = .systemBackground
+           tabBar.tintColor = .label
+           setupVCs()
+        
+    }
+    
+    fileprivate func createNavController(for rootViewController: UIViewController,
+                                         title: String,
+                                         image: UIImage) -> UIViewController {
+          let navController = UINavigationController(rootViewController: rootViewController)
+          navController.tabBarItem.title = title
+          navController.tabBarItem.image = image
+          navController.navigationBar.prefersLargeTitles = true
+          rootViewController.navigationItem.title = title
+          return navController
+      }
+    
+    func setupVCs() {
+          viewControllers = [
+              createNavController(for: PeopleViewController(), title: NSLocalizedString("Chat", comment: ""), image: UIImage(systemName: "message")!),
+              createNavController(for: ProfileViewController(), title: NSLocalizedString("Profile", comment: ""), image: UIImage(systemName: "person")!)
+          ]
+      }
+}
